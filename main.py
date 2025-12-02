2d5b2c3dee88fdba62633351f512e5cb9f5d3554
rags7505 <darurisreeraghava@gmail.com>
2025-12-02 15:30:30 +0000

    small functionality added

---
+++ main.py
@@ -0,0 +1,21 @@
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from logger import log_state
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    print("Starting Asteroids with pygame version: ",pygame.version.ver)
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+
+
+if __name__ == "__main__":
+    main()
